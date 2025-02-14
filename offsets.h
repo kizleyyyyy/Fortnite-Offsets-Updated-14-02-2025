c4d57b08a085945791538febfc028a981cac79d6
NoxarTM <kizley706@gmail.com>
2025-02-14 16:27:33 +0000

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,34 @@
+#pragma once
+#define UWORLD 0x16348C08
+#define GAME_INSTANCE 0x1F8
+#define LOCAL_PLAYERS 0x38
+#define PLAYER_CONTROLLER 0x30
+#define LOCAL_PAWN 0x350 
+#define PAWN_PRIVATE 0x320 
+#define ROOT_COMPONENT 0x1B0
+#define RELATIVE_LOCATION 0x138
+#define PLAYER_STATE 0x2C8
+#define TEAM_INDEX 0x1259  
+#define GAME_STATE 0x180
+#define PLAYER_ARRAY 0x2C0
+#define MESH 0x328
+#define COMPONENT_TO_WORLD 0x1E0
+#define BONE_ARRAY 0x5A8
+#define BONE_ARRAY_CACHE 0x5B8
+#define LAST_SUBMIT_TIME 0x2E8
+#define LAST_SUBMIT_TIME_ON_SCREEN 0x2F0
+#define VELOCITY 0x180
+#define CURRENT_WEAPON 0xa80
+#define WEAPON_DATA 0x5E8 
+#define DISPLAY_NAME 0x40
+#define HABANERO_COMPONENT 0xa38
+#define RANKED_PROGRESS 0xd0 // updated, enjoy
+#define PLATFORM 0x430 // updated, enjoy
+#define WEAPON_NAME 0x40
+#define PLATFORM  0x430 // For Platform Icons/ESP
+#define PERSISTENT_LEVEL 0x40 // For Exploits, Updated
+#define ISDYING 0x718 // For Exploits, Updated / Skeleton,Boxes,Lines
+#define PERSISTENT_LEVEL 0x40 // For Exploits, Updated
+#define GlobalAnimRateScale 0xa20 //For Exploits, Updated
+#define bADSWhileNotOnGround 0x5869 // For Exploits, Updated
+#define CustomTimeDilation 0x600 // For Exploits, Updated
